7c718eddd22206d2f265d5a26e2a0188053d3a00
bmkamath2000 <bmkamath2000@gmail.com>
2020-07-18 20:46:03 +0530

    Plaza checkin
    checkin

---
+++ Plaza/Plaza/plaza/Object.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using ShadowEngine;
+using ShadowEngine.ContentLoading;
+using Tao.OpenGl; 
+
+namespace Plaza
+{
+    class Object
+    {
+        ModelContainer N;
+
+        public void create()
+        {
+
+            N = ContentManager.GetModelByName("Sofa 3 seat.3DS");
+            N.CreateDisplayList();
+        }
+        public void Draw()
+        {
+            Gl.glPushMatrix();
+            Gl.glTranslatef(0f, 0f, 75f);
+            Gl.glScalef(0.05f, 0.05f, 0.05f);
+            N.DrawWithTextures();
+            Gl.glPopMatrix();
+        }
+    }
+}
